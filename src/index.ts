1f55bb6ad6d96cebdf6b3c3ee40943322f922369
Reelecks <robinlandais75@yahoo.com>
2023-01-25 11:34:52 +0100

    Init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import userRoutes from './routes/user'
+import todoListRoutes from './routes/todoList'
+import todoItemRoutes from './routes/todoItem'
+import db from './db'
+import { protect } from './modules/auth'
+import { signIn, createNewUser } from './handlers/user'
+import config from './config'
+dotenv.config()
+
+const app = express()
+const PORT = config.port
+app.use(express.json())
+app.listen(PORT, ()=>{
+    console.log('Listening on localhost:', PORT)
+})
+
+app.get('/', (req, res) =>{
+    console.log(process.env.DATABASE_URL)
+    res.status(200).json({message:  'hello'})
+})
+
+app.use('/api',protect,  [userRoutes])
+app.use('/api/todo', protect, [todoListRoutes])
+app.use('/api/todoItem', protect, [todoItemRoutes])
+app.post('/signup', createNewUser)
+app.post('/signin', signIn)
---
+++ src/modules/auth.ts
@@ -0,0 +1,46 @@
+import { User } from '@prisma/client'
+import { RequestHandler } from 'express';
+import jwt from "jsonwebtoken";
+import * as bcrypt from 'bcrypt'
+//pnpm add @types/bcrypt -d permet d'importer les types
+//token a stocker dans les cookies
+//fetch headers -> autorisations -> bearer token
+
+export const createJWT = (user: User) => {
+    const token = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET as string // force le type
+    );
+    return token;
+}
+//Permet de vérifier sic l'utilisateur envoie un token valide
+export const protect: RequestHandler = (req, res, next) => {
+    const bearer = req.headers.authorization
+    if (!bearer) {
+        return res.status(401).json({ message: 'Not authorized' })
+    }
+    const [, token] = bearer.split(' ') // récupère le token 
+    if (!token) {
+        return res.status(401).json({ message: 'Not authorized' })
+    }
+    try {
+        if (typeof process.env.JWT_SECRET !== 'string') {
+            return res.status(401).json({ message: 'Not authorized' })
+        }
+        const payload = jwt.verify(token, process.env.JWT_SECRET as string) as User
+        req.user = payload
+        next()
+    } catch (e) {
+        return res.status(401).json({ message: 'Not authorized' })
+    }
+
+}
+
+export const comparePassword = (password: string, hash: string) => {
+    return bcrypt.compare(password, hash)
+}
+
+
+export const hashPassword = (password: string) => {
+    return bcrypt.hash(password, 10)
+}
